41ac285dcac9e79808b53c5a5b4403cef273a44e
Mhmmdfhkri <mufik2002@gmail.com>
2025-02-02 18:09:12 +0800

    Create comment API

--- client/src/pages/blogs/comments/PostAComment.jsx
+++ client/src/pages/blogs/comments/PostAComment.jsx
@@ -1,14 +1,42 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux"
+import { usePostCommentMutation } from "../../../redux/features/comments/commentApi";
 
 const PostAComment = () => {
   const { id } = useParams();
   const [comment, setComment] = useState('')
-    // TODO : handle Posting funcsionality later
+  const {user} = useSelector((state) => state.auth);
+  const navigate = useNavigate()
+  // console.log(user)
+  const [postComment] = usePostCommentMutation();
+  
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    if(!user){
+      alert('Please Login to Comment on this post')
+      navigate("/login")
+      return;
+    }
+    const newComment = {
+      comment: comment,
+      user: user?._id,
+      postId: id
+    };
+    // console.log(newComment)
+    try {
+      const response = await postComment(newComment).unwrap();
+      console.log(response)
+    } catch (error) {
+      alert('An Error occured posting Comment')
+    }
+  } 
+
+  
   return (
     <div className="mt-8">
       <h3 className="text-lg font-medium mb-8">Leave a comment</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <textarea name="text"
         value={comment}
         onChange={(e)=>setComment(e.target.value)}
